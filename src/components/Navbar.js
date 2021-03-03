b4b09e251bbbc52b8529ed86190346183563deb2
Lucas Benitez <lucasbenitez323@gmail.com>
2021-03-03 01:23:53 -0300

    Agregado de navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon } from "mdbreact";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class NavbarPage extends Component {
+state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
+}
+
+render() {
+  return (
+    <Router>
+      <MDBNavbar color="teal lighten-2" dark expand="md">
+        <MDBNavbarBrand>
+          <strong className="white-text">Mercado Privado</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav left>
+            <MDBNavItem active>
+              <MDBNavLink to="#!">Incio</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBDropdown>
+                <MDBDropdownToggle nav caret>
+                  <div className="d-none d-md-inline">Categorias</div>
+                </MDBDropdownToggle>
+                <MDBDropdownMenu className="dropdown-default">
+                  <MDBDropdownItem href="#!">Celulares</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Auriculares</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Componentes de PC</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Parlantes</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right>
+         
+            <MDBNavItem>
+              <MDBNavLink className="waves-effect waves-light" to="#!">
+              <MDBIcon icon="shopping-cart" />
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    </Router>
+    );
+  }
+}
+
+export default NavbarPage;
