b9360600ff2503e8ea84b1c585b222c7bb0ad6ba
Lucas Benitez <lucasbenitez323@gmail.com>
2021-04-05 19:51:02 -0300

    Corrección del item detail

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,34 +1,59 @@
-  import { useState } from 'react';
-  import { MDBBtn, MDBCard, MDBCardBody, MDBCardText, MDBCol, MDBContainer, MDBInput,MDBNavLink } from 'mdbreact';
-  const ItemCount = ({ min=0, max, value=0, onAdd, onSubstract}) => {
-    /* const initial = 0
+import { useState } from "react";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardText,
+  MDBCol,
+  MDBContainer,
+  MDBInput,
+  MDBNavLink,
+} from "mdbreact";
+const ItemCount = ({ min = 0, max, value = 1, onAdd, onSubstract }) => {
+  /* const initial = 0
     const [stockActual, setStockActual] = useState(initial);
     const stock = item.stock
     const stockDisponible = stock - stockActual; */
-    return (
-  
-        <MDBCol style={{ maxWidth: "30rem" }}>
-        <MDBCard>
-          <MDBCardBody>
-            {/* <MDBCardText>Cantidad de stock: {stockDisponible}</MDBCardText>  */}
-            {/* <MDBCardText> Stock disponible: {stockActual}</MDBCardText> */}
-            <MDBInput type="number" label={value} min="0" max={max}></MDBInput>
-            <MDBBtn variant="teal"  onClick={(e) => onSubstract(e)} disabled={value <= min}>-</MDBBtn>
-            <MDBBtn variant="teal"  onClick={(e) => onAdd(e)} disabled={value >= max}>+</MDBBtn>
-            
-            {/* <MDBBtn variant="teal"  onClick={(e) =>{ onAdd(e, stockActual); setStockActual(0)}}>Añadir al carrito</MDBBtn> */}
-            {/* <MDBContainer className="d-flex"> */}
-            {/* <MDBNavLink to="/cart" className={`btn--big ${stock || 'disabled'}`}  >
+  return (
+    <MDBCol style={{ maxWidth: "30rem" }}>
+      <MDBCard>
+        <MDBCardBody>
+          {/* <MDBCardText>Cantidad de stock: {stockDisponible}</MDBCardText>  */}
+          {/* <MDBCardText> Stock disponible: {stockActual}</MDBCardText> */}
+
+          <input
+            className="form-control"
+            id="formGroupExampleInput"
+            type="number" label={value} min="0" max={max}
+            placeholder={value}
+          />
+
+          <MDBBtn
+            variant="teal"
+            onClick={(e) => onSubstract(e)}
+            disabled={value <= min}
+          >
+            -
+          </MDBBtn>
+          <MDBBtn
+            variant="teal"
+            onClick={(e) => onAdd(e)}
+            disabled={value >= max}
+          >
+            +
+          </MDBBtn>
+
+          {/* <MDBBtn variant="teal"  onClick={(e) =>{ onAdd(e, stockActual); setStockActual(0)}}>Añadir al carrito</MDBBtn> */}
+          {/* <MDBContainer className="d-flex"> */}
+          {/* <MDBNavLink to="/cart" className={`btn--big ${stock || 'disabled'}`}  >
               <MDBBtn variant="teal"  onClick={(e) =>{ onAdd(e, stockActual);}}> Comprar</MDBBtn>
               </MDBNavLink> */}
-            
-            {/* </MDBContainer> */}
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    );
-  };
 
+          {/* </MDBContainer> */}
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+  );
+};
 
-
-  export default ItemCount;
+export default ItemCount;
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,25 +1,58 @@
 import ItemCount from "../ItemCount/ItemCount";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { MDBContainer, MDBNavLink, MDBRow, MDBCol,MDBBtn } from "mdbreact";
+import { MDBContainer, MDBNavLink, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 function ItemDetail({ item }) {
   const initial = 0;
   const [stock, setStock] = useState(initial);
   const availableStock = item.stock - stock;
 
   const Stock = () => {
-      return (
-          <>
-              <ItemCount  max={item.stock} value={stock} onAdd={onAdd} onSubstract={onSubstract} />
-              
-          </>
-              
-      )
-  }
+    return (
+      <>
+        <ItemCount
+          max={item.stock}
+          min="0"
+          value={stock}
+          onAdd={onAdd}
+          onSubstract={onSubstract}
+        />
+      </>
+    );
+  };
   const NoStock = () => {
     return <h3> No stock available </h3>;
   };
-
+  const Finish = () => {
+    return (
+      <>
+        <MDBNavLink to="/cart" className="text-center">
+          <MDBBtn variant="teal">Finalizar compra</MDBBtn>
+        </MDBNavLink>
+      </>
+    );
+  };
+  const NoFinalizar = () => {
+    return "";
+  };
+  const AñadirCarrito = () => {
+    return (
+      <>
+        <MDBNavLink to="/cart" className="text-center">
+          <MDBBtn variant="teal">Añadir al carrito</MDBBtn>
+        </MDBNavLink>
+      </>
+    );
+  };
+  const NoAñadirCarrito = () => {
+    return (
+      <>
+        <MDBNavLink to="/cart" className="text-center">
+          <MDBBtn variant="teal" disabled>Añadir al carrito</MDBBtn>
+        </MDBNavLink>
+      </>
+    );
+  };
   const onAdd = (e) => {
     e.preventDefault();
     if (stock > item.stock) {
@@ -39,7 +72,8 @@ function ItemDetail({ item }) {
   };
 
   const IsAvailable = item.stock > 0 ? Stock : NoStock;
-
+  const FinalizarCompra = stock > 0 ? Finish : NoFinalizar;
+  const SePuedeAñadir = stock > 0 ? AñadirCarrito : NoAñadirCarrito;
   return (
     <MDBContainer id={item.id}>
       <MDBRow>
@@ -52,14 +86,27 @@ function ItemDetail({ item }) {
           <IsAvailable />
         </MDBCol>
         <MDBCol>
-          <h1 className="h1-responsive font-weight-bold text-center my-5">{item.title}</h1>
-          <h2 className="text-center w-responsive mx-auto mb-3">Stock : {availableStock}</h2>
-          <div className="text-center w-responsive mx-auto mb-5">{item.description}</div>
-          <div className="text-center w-responsive mx-auto mb-5">{item.fullDescription}</div>
-          <h2 className="text-center w-responsive mx-auto mb-3 font-weight-bold">$ {item.price}</h2>
-          <MDBNavLink to="/cart" className="text-center">
-                <MDBBtn variant="teal">Añadir al carrito</MDBBtn>
-          </MDBNavLink>
+          <h1 className="h1-responsive font-weight-bold text-center my-5">
+            {item.title}
+          </h1>
+          <h2 className="text-center w-responsive mx-auto mb-3">
+            Stock : {availableStock}
+          </h2>
+          <div className="text-center w-responsive mx-auto mb-5">
+            {item.description}
+          </div>
+          <div className="text-center w-responsive mx-auto mb-5">
+            {item.fullDescription}
+          </div>
+          <h2 className="text-center w-responsive mx-auto mb-3 font-weight-bold">
+            $ {item.price}
+          </h2>
+        <MDBContainer className="d-flex">
+        <SePuedeAñadir/>
+          <FinalizarCompra />
+        </MDBContainer>
+          
+
         </MDBCol>
       </MDBRow>
     </MDBContainer>
